15d82a220b7f1a7bf1ec99ee971384a2f4ccb769
Alexander <alexander.thompson.124@gmail.com>
2022-09-06 01:31:16 +0100

    Added firebase to the project

---
+++ src/components/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsCa0fl5qG4FMDY3zOr7Gqy6eNA8ybYsc",
+  authDomain: "ticket-system-265d4.firebaseapp.com",
+  projectId: "ticket-system-265d4",
+  storageBucket: "ticket-system-265d4.appspot.com",
+  messagingSenderId: "497194492441",
+  appId: "1:497194492441:web:a3716fb166fda273d88088"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
